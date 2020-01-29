c6ed831beb6f29ad6a8fd00082ef34c82622b143
kath <iamkate4@gmail.com>
2020-01-29 11:09:21 +0200

    loaded scss and babel palugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
  
 module.exports = {
     entry: './src/index.js',
@@ -6,4 +7,34 @@ module.exports = {
         path: path.resolve(__dirname, 'docs'),
         filename: 'js/main.js',
     },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            // ...additional rules...
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css",
+        }),
+    ],
+    watch: true,
+  
 };
