3061bbdf35634dca20f89d5ad59c83dc6fa401e1
kath <iamkate4@gmail.com>
2020-01-30 17:53:12 +0200

    added all image loader function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '../')] = r(item); });
+    return images;
+  }
+  
+const images = importAll(require.context('./img', false, /\.(png|jpe?g|svg)$/));
 import './scss/main.scss';
 
 console.log("Hello, world!");
